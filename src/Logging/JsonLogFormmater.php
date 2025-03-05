beb2ba86bc2744d49dad4fd610e94ed7e327c8b0
Rander <rfreitas@uplexis.com.br>
2025-03-05 20:27:50 -0300

    chore: first commit

---
+++ src/Logging/JsonLogFormmater.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Ecommerce\Common\Logging;
+
+use Monolog\Formatter\NormalizerFormatter;
+use Throwable;
+
+class JsonLogFormmater extends NormalizerFormatter
+{
+    private const SEPARATOR = '==================================================================================================='; // Se
+
+    public function __construct()
+    {
+        parent::__construct('Y-m-d H:i:s');
+    }
+
+    public function format(array $record): string
+    {
+        $exception = $record['context']['exception'] ?? null;
+
+        $log = [
+            'requestId' => request()->header('X-Request-Id'),
+            'timestamp' => $record['datetime']->format($this->dateFormat),
+            'level' => $record['level_name'],
+            'service' => config('app.name'),
+            'environment' => config('app.env'),
+            'message' => $record['message'],
+            'file' => $this->getFile($record, $exception),
+            'line' => $this->getLine($record, $exception),
+            'context' => $record['context'] ?? [],
+            'stacktrace' => $this->formatStackTrace($exception),
+        ];
+
+
+        return json_encode($log, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT)
+            . PHP_EOL . self::SEPARATOR . PHP_EOL;
+    }
+
+    private function formatStackTrace(?Throwable $exception): ?array
+    {
+        if (!$exception) {
+            return null;
+        }
+
+        $traceLines = []; // Adiciona cabeçalho igual ao log padrão do Laravel
+        foreach ($exception->getTrace() as $index => $trace) {
+            $traceLines[] = sprintf(
+                "#%d %s(%s): %s%s%s(%s)",
+                $index,
+                $trace['file'] ?? '[internal]',
+                $trace['line'] ?? '?',
+                $trace['class'] ?? '',
+                $trace['type'] ?? '',
+                $trace['function'] ?? '',
+                $this->formatArgs($trace['args'] ?? [])
+            );
+        }
+
+        return $traceLines;
+    }
+
+    private function formatArgs(array $args): string
+    {
+        return implode(', ', array_map(fn ($arg) => gettype($arg), $args)); // Mostra apenas os tipos dos argumentos
+    }
+
+    private function getFile(array $record, ?Throwable $exception): ?string
+    {
+        return $record['extra']['file'] ?? ($exception ? $exception->getFile() : null);
+    }
+
+    private function getLine(array $record, ?Throwable $exception): ?int
+    {
+        return $record['extra']['line'] ?? ($exception ? $exception->getLine() : null);
+    }
+}
