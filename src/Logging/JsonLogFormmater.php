a8c9396bf2fdaf6d4aa97bd7e57119cabfced8f4
Rander <rfreitas@uplexis.com.br>
2025-03-06 09:45:46 -0300

    refactor: change JsonLogFormatter to extends JsonFormatter

--- src/Logging/JsonLogFormmater.php
+++ src/Logging/JsonLogFormmater.php
@@ -2,14 +2,14 @@
 
 namespace Ecommerce\Common\Logging;
 
-use Monolog\Formatter\NormalizerFormatter;
+use Monolog\Formatter\JsonFormatter;
 use Throwable;
 
-class JsonLogFormmater extends NormalizerFormatter
+class JsonLogFormatter extends JsonFormatter
 {
     public function __construct()
     {
-        parent::__construct('Y-m-d H:i:s');
+        parent::__construct(JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
     }
 
     public function format(array $record): string
@@ -17,21 +17,19 @@ class JsonLogFormmater extends NormalizerFormatter
         $exception = $record['context']['exception'] ?? null;
 
         $log = [
-            'requestId' => request()->header('X-Request-Id'),
-            'timestamp' => $record['datetime']->format($this->dateFormat),
-            'level' => $record['level_name'],
-            'service' => config('app.name'),
+            'requestId'   => request()->header('X-Request-Id'),
+            'timestamp'   => $record['datetime']->format($this->dateFormat),
+            'level'       => $record['level_name'],
+            'service'     => config('app.name'),
             'environment' => config('app.env'),
-            'message' => $record['message'],
-            'file' => $this->getFile($record, $exception),
-            'line' => $this->getLine($record, $exception),
-            'context' => $record['context'] ?? [],
-            'stacktrace' => $this->formatStackTrace($exception),
+            'message'     => $record['message'],
+            'file'        => $this->getFile($record, $exception),
+            'line'        => $this->getLine($record, $exception),
+            'context'     => $record['context'] ?? [],
+            'stacktrace'  => $this->formatStackTrace($exception),
         ];
 
-
-        return json_encode($log, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE)
-            . PHP_EOL;
+        return parent::format($log);
     }
 
     private function formatStackTrace(?Throwable $exception): ?array
@@ -40,35 +38,36 @@ class JsonLogFormmater extends NormalizerFormatter
             return null;
         }
 
-        $traceLines = []; // Adiciona cabeçalho igual ao log padrão do Laravel
-        foreach ($exception->getTrace() as $index => $trace) {
-            $traceLines[] = sprintf(
-                "#%d %s(%s): %s%s%s(%s)",
-                $index,
-                $trace['file'] ?? '[internal]',
-                $trace['line'] ?? '?',
-                $trace['class'] ?? '',
-                $trace['type'] ?? '',
-                $trace['function'] ?? '',
-                $this->formatArgs($trace['args'] ?? [])
-            );
-        }
-
-        return $traceLines;
+        return array_map(
+            function ($trace, $index) {
+                return sprintf(
+                    "#%d %s(%s): %s%s%s(%s)",
+                    $index,
+                    isset($trace['file']) ? $trace['file'] : '[internal]',
+                    isset($trace['line']) ? $trace['line'] : '?',
+                    isset($trace['class']) ? $trace['class'] : '',
+                    isset($trace['type']) ? $trace['type'] : '',
+                    isset($trace['function']) ? $trace['function'] : '',
+                    $this->formatArgs(isset($trace['args']) ? $trace['args'] : [])
+                );
+            },
+            $exception->getTrace(),
+            array_keys($exception->getTrace())
+        );
     }
 
     private function formatArgs(array $args): string
     {
-        return implode(', ', array_map(fn ($arg) => gettype($arg), $args)); // Mostra apenas os tipos dos argumentos
+        return implode(', ', array_map('gettype', $args));
     }
 
     private function getFile(array $record, ?Throwable $exception): ?string
     {
-        return $record['extra']['file'] ?? ($exception ? $exception->getFile() : null);
+        return isset($record['extra']['file']) ? $record['extra']['file'] : ($exception ? $exception->getFile() : null);
     }
 
     private function getLine(array $record, ?Throwable $exception): ?int
     {
-        return $record['extra']['line'] ?? ($exception ? $exception->getLine() : null);
+        return isset($record['extra']['line']) ? $record['extra']['line'] : ($exception ? $exception->getLine() : null);
     }
 }
